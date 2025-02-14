bb61fb499b9c38573ccb51794e5369b5da665bf3
equalDelight <73264204+equalDelight@users.noreply.github.com>
2025-02-14 13:22:06 +0700

    Update AssemblyStoreAssembly.cs
    Simplify some method

--- AssemblyStore/AssemblyStoreAssembly.cs
+++ AssemblyStore/AssemblyStoreAssembly.cs
@@ -13,13 +13,12 @@ namespace Xamarin.Android.AssemblyStore
         public uint DebugDataSize { get; }
         public uint ConfigDataOffset { get; }
         public uint ConfigDataSize { get; }
-
         public uint Hash32 { get; set; }
         public ulong Hash64 { get; set; }
-        public string Name { get; set; } = String.Empty;
+        public string Name { get; set; } = string.Empty;
         public uint RuntimeIndex { get; set; }
-
         public AssemblyStoreReader Store { get; }
+
         public string DllName => MakeFileName("dll");
         public string PdbName => MakeFileName("pdb");
         public string ConfigName => MakeFileName("dll.config");
@@ -27,7 +26,6 @@ namespace Xamarin.Android.AssemblyStore
         internal AssemblyStoreAssembly(BinaryReader reader, AssemblyStoreReader store)
         {
             Store = store;
-
             DataOffset = reader.ReadUInt32();
             DataSize = reader.ReadUInt32();
             DebugDataOffset = reader.ReadUInt32();
@@ -36,74 +34,58 @@ namespace Xamarin.Android.AssemblyStore
             ConfigDataSize = reader.ReadUInt32();
         }
 
-        public void ExtractImage(string outputDirPath, string? fileName = null, bool decompress = true)
+        public void Extract(string outputDirPath, string extension, string? fileName = null, bool decompress = false)
         {
-            var outputFilePath = MakeOutputFilePath(outputDirPath, "dll", fileName);
+            var outputFilePath = MakeOutputFilePath(outputDirPath, extension, fileName);
             Store.ExtractAssemblyImage(this, outputFilePath);
-            if (decompress)
+            if (decompress && extension == "dll")
+            {
                 DecompressDll(outputFilePath);
+            }
         }
 
-        public static void DecompressDll(string path)
+        public void Extract(Stream output, string extension)
+        {
+            switch (extension)
+            {
+                case "dll":
+                    Store.ExtractAssemblyImage(this, output);
+                    break;
+                case "pdb":
+                    Store.ExtractAssemblyDebugData(this, output);
+                    break;
+                case "dll.config":
+                    Store.ExtractAssemblyConfig(this, output);
+                    break;
+            }
+        }
+
+        private static void DecompressDll(string path)
         {
             var compressedData = File.ReadAllBytes(path);
-            var header = Encoding.ASCII.GetString(compressedData[0..4]);
-            if (header != "XALZ")
+            if (Encoding.ASCII.GetString(compressedData[0..4]) != "XALZ")
                 return;
 
             var unpackLength = BitConverter.ToInt32(compressedData[8..12]);
             var payload = compressedData[12..];
-            byte[] decompressedData = new byte[unpackLength];
+            var decompressedData = new byte[unpackLength];
             LZ4Codec.Decode(payload, decompressedData);
             File.WriteAllBytes(path, decompressedData);
-            //Console.WriteLine($"Decompressed LZ4: {path}");
         }
 
-        public void ExtractImage(Stream output)
-        {
-            Store.ExtractAssemblyImage(this, output);
-        }
-
-        public void ExtractDebugData(string outputDirPath, string? fileName = null)
-        {
-            Store.ExtractAssemblyDebugData(this, MakeOutputFilePath(outputDirPath, "pdb", fileName));
-        }
-
-        public void ExtractDebugData(Stream output)
-        {
-            Store.ExtractAssemblyDebugData(this, output);
-        }
-
-        public void ExtractConfig(string outputDirPath, string? fileName = null)
-        {
-            Store.ExtractAssemblyConfig(this, MakeOutputFilePath(outputDirPath, "dll.config", fileName));
-        }
-
-        public void ExtractConfig(Stream output)
-        {
-            Store.ExtractAssemblyConfig(this, output);
-        }
-
-        string MakeOutputFilePath(string outputDirPath, string extension, string? fileName)
+        private string MakeOutputFilePath(string outputDirPath, string extension, string? fileName)
         {
             return Path.Combine(outputDirPath, MakeFileName(extension, fileName));
         }
 
-        string MakeFileName(string extension, string? fileName = null)
+        private string MakeFileName(string extension, string? fileName = null)
         {
-            if (String.IsNullOrEmpty(fileName))
+            fileName ??= Name;
+            if (string.IsNullOrEmpty(fileName))
             {
-                fileName = Name;
-
-                if (String.IsNullOrEmpty(fileName))
-                {
-                    fileName = $"{Hash32:x}_{Hash64:x}";
-                }
-
-                fileName = $"{fileName}.{extension}";
+                fileName = $"{Hash32:x}_{Hash64:x}";
             }
-
-            return fileName!;
+            return $"{fileName}.{extension}";
         }
     }
 }
